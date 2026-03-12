32c5e7d881cb006010ceb9c29e86f59dfb1d0859
Wiktor <wiktor-wosiek@wp.pl>
2026-03-12 11:07:50 +0100

    Implementacja modelu Vehicle

--- FleetManager/Models/Vehicle.cs
+++ FleetManager/Models/Vehicle.cs
@@ -1,7 +1,16 @@
 namespace FleetManager.Models;
 
-public class Vehicle 
+public enum VehicleStatus
 {
-    
+    Available,
+    InRoute,
+    Service
 }
-// 
+
+public class Vehicle
+{
+    public string Name { get; set; } = string.Empty;
+    public string LicensePlate { get; set; } = string.Empty;
+    public double FuelLevel { get; set; }
+    public VehicleStatus Status { get; set; }
+}
